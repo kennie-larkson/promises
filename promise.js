9f5bdff1de926a5f5f90b354c8c66c16115d09ed
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-20 22:10:01 +0100

    handled success and failure

--- promise.js
+++ promise.js
@@ -1,5 +1,3 @@
-//this creates a Promise using the Promise constructor which takes a callback
-//function with two parameters (resolve and reject) as arguments
 const myPromise = new Promise((resolve, reject)=>{
     let success;
     if(success){
